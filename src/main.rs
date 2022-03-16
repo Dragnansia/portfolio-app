a61544c742327895baa0830d7c4ad4235ce5634e
Dragnansia <romuald.auc.git@protonmail.com>
2022-03-16 18:54:48 +0100

    basic project initialisation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use eframe::{egui, epi, run_native, NativeOptions};
+
+struct Portfolio;
+
+impl epi::App for Portfolio {
+    fn update(&mut self, _: &egui::Context, _: &epi::Frame) {}
+
+    fn name(&self) -> &str {
+        "Portfolio App"
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    run_native(Box::new(Portfolio), NativeOptions::default());
 }
